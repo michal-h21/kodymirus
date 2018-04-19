e43cd568b6dfff80936d4704364525753aeb1b78
Michal Hoftich <michal.h21@gmail.com>
2018-04-19 17:49:57 +0200

    basic modules

--- src/blog.lua
+++ src/blog.lua
@@ -1,2 +1,10 @@
-local lettersmith = require "lettersmith"
+local lettersmith = require("lettersmith")
+local transducers = require "lettersmith.transducers"
+local rss = require "atom"
+local map = transducers.map
+local reduce = transducers.reduce
+local filter  = transducers.filter
+local lazy = require "lettersmith.lazy"
+local transform = lazy.transform
+local transformer = lazy.transformer
 
