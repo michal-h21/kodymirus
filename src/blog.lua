a66ac06dc5633fefaeb48aeef0d5c7d8433c5db9
Michal Hoftich <michal.h21@gmail.com>
2018-04-20 14:15:27 +0200

    lettersmith build works

--- src/blog.lua
+++ src/blog.lua
@@ -1,3 +1,4 @@
+package.path = "?.lua;src/?.lua;"..package.path
 local lettersmith = require("lettersmith")
 local transducers = require "lettersmith.transducers"
 -- local rss = require "atom"
@@ -5,6 +6,50 @@ local map = transducers.map
 local reduce = transducers.reduce
 local filter  = transducers.filter
 local lazy = require "lettersmith.lazy"
+local merge = require("lettersmith.table_utils").merge
 local transform = lazy.transform
 local transformer = lazy.transformer
+local templates = require "template"
+local base_template = templates.base
+
+
+local paths = lettersmith.paths("build")
+local comp = require("lettersmith.transducers").comp
+
+-- local render_mustache = require("lettersmith.mustache").choose_mustache
+
+local make_transformer = function(fn)
+  return transformer(map(fn))
+end
+
+local make_filter = function(reg)
+  return transformer(filter(function(doc)
+    local fn = doc.relative_filepath
+    return fn:match(reg)
+  end
+  ))
+end
+
+local html_filter = make_filter("html$")
+
+local add_defaults = make_transformer(function(doc)
+  -- potentially add default variables
+  return doc
+end)
+
+local apply_template = make_transformer(function(doc)
+  local rendered = base_template(doc)
+  return merge(doc, {contents = rendered})
+end)
+
+local builder = comp(lettersmith.docs)
+local html_builder = comp(
+  apply_template,
+  add_defaults,
+  html_filter,
+  lettersmith.docs
+)
+
+
+lettersmith.build("www", builder(paths), html_builder(paths))
 
---
+++ src/template.lua
@@ -0,0 +1,33 @@
+local h5tk = require "h5tk"
+local template = {}
+
+local h = h5tk.init(true)
+local html, head, body, title, link, article = h.html, h.head, h.body, h.title, h.link, h.article
+
+local function styles(s)
+  local t = {}
+  for _, style in ipairs(s) do
+    table.insert(t, link {rel="stylesheet", type="text/css", href="css/"..  style})
+  end
+  return t
+end
+
+function template.base(doc)
+  return "<!DOCTYPE html>\n" .. (h.emit(
+  html { lang="en", 
+    head {
+      h.meta {charset="utf-8"},
+      title { doc.title },
+      (styles(doc.styles))
+    },
+    body {
+      article {
+        doc.contents
+      }
+    }
+  }
+  ))
+
+end
+
+return template
